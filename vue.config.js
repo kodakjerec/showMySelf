d517e0cf10fd4757e7e6d651555900bda0f40986
kodakjerec <kodakjerec@gmail.com>
2022-12-28 11:43:50 +0800

    update

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("@vue/cli-service");
 
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === "production" ? "/showMySelf" : "",
+  publicPath: process.env.NODE_ENV === "production" ? "/showMySelf/dist" : "",
   configureWebpack: {},
   chainWebpack: (config) => {
     config.module
